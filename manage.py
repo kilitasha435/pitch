a8f31f0722ba140cfa680f695a20bb8117c8b243
kevin-kili <kilikevin435@gmail.com>
2020-05-08 01:15:03 +0300

    added the auth folder

--- manage.py
+++ manage.py
@@ -1,12 +1,15 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import User
+from  flask_migrate import Migrate, MigrateCommand
+from app.models import User,Pitch
 
 # Creating app instance
 app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 @manager.command
 def test():
@@ -15,5 +18,8 @@ def test():
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User,Pitch= Pitch)
 if __name__ == '__main__':
     manager.run()
