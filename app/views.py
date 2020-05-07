e77fba8da8393921841c9a4d33c83a8384609982
kevin-kili <kilikevin435@gmail.com>
2020-05-07 23:37:09 +0300

    added the macros.html

--- app/views.py
+++ app/views.py
@@ -1,7 +1,14 @@
 from flask import render_template,request,redirect,url_for
 from app import app
 from .request import get_movies,get_movie,search_movie
+from .forms import ReviewForm
+from .models import review
 
+Review = review.Review
+
+
+
+# Views
 @app.route('/')
 def index():
 
@@ -23,6 +30,7 @@ def index():
     else:
         return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
 
+
 @app.route('/movie/<int:id>')
 def movie(id):
 
@@ -31,8 +39,11 @@ def movie(id):
     '''
     movie = get_movie(id)
     title = f'{movie.title}'
+    reviews = Review.get_reviews(movie.id)
 
-    return render_template('movie.html',title = title,movie = movie)
+    return render_template('movie.html',title = title,movie = movie,reviews = reviews)
+
+
 
 @app.route('/search/<movie_name>')
 def search(movie_name):
@@ -44,3 +55,23 @@ def search(movie_name):
     searched_movies = search_movie(movie_name_format)
     title = f'search results for {movie_name}'
     return render_template('search.html',movies = searched_movies)
+
+
+@app.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+def new_review(id):
+
+    form = ReviewForm()
+
+    movie = get_movie(id)
+
+    if form.validate_on_submit():
+        title = form.title.data
+        review = form.review.data
+
+        new_review = Review(movie.id,title,movie.poster,review)
+        new_review.save_review()
+
+        return redirect(url_for('movie',id = movie.id ))
+
+    title = f'{movie.title} review'
+    return render_template('new_review.html',title = title, review_form=form, movie=movie)
