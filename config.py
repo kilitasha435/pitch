1e049c7d4950554e7fed2c603f87c2f9be853cb6
kevin-kili <kilikevin435@gmail.com>
2020-05-08 09:52:09 +0300

    changed to production

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://kevin:kilitasha@123@localhost/pitch"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class TestConfig(Config):
     '''
