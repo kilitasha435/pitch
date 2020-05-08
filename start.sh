738ebfd2f80a36d60cdf3d4536096d64b0d98896
kevin-kili <kilikevin435@gmail.com>
2020-05-08 09:32:30 +0300

    added readme

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 export MAIL_USERNAME='kilikevin435@gmail.com'
 export  MAIL_PASSWORD='0717464934'
-export SECRET_KEY = 'kilitasha'
+export SECRET_KEY='kilitasha'
 
 SQLALCHEMY_DATABASE_URI='DATABASE_URL'
 
-python3.8 manage.py server
+python3.6 manage.py server
